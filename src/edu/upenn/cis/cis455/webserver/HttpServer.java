a0d3cf343b97dd7e131d8ef7db6ac1d86e53fbec
Brian Shi <brishi@seas.upenn.edu>
2015-09-17 20:16:38 -0400

    Initial commit

--- src/edu/upenn/cis/cis455/webserver/HttpServer.java
+++ src/edu/upenn/cis/cis455/webserver/HttpServer.java
@@ -1,10 +1,13 @@
 package edu.upenn.cis.cis455.webserver;
 
 class HttpServer {
-  
-  public static void main(String args[])
-  {
-    /* your code here */
-  }
-  
+
+    public HttpServer() {
+    }
+
+    public static void main(String args[]) {
+          /* DAEMON THREAD GOES HERE */
+        /* your code here */
+    }
+
 }
