6255447f8dd100a556826e7468f03d1b549fa122
Brian Shi <brishi@seas.upenn.edu>
2015-09-21 20:01:48 -0400

    Updated README, build.xml, and runserver.sh script.

--- runserver.sh
+++ runserver.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-java -classpath lib/*:bin/. edu.upenn.cis.cis455.webserver.HttpServer 8080 /home/cis455/workspace/HW1/www &
+java -classpath bin:lib/log4j-1.2.9.jar edu.upenn.cis.cis455.webserver.HttpServer 8080 www/ &
 sleep 2
 curl http://localhost:8080/control
 curl http://localhost:8080/shutdown
